d86a19cfd9e2da1ac50b395e659037a6da9ff08c
AllynH <AllynH@gmail.com>
2016-11-22 11:18:49 +0000

    Updating repo with Part V: User Logins

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
 #!flask\Scripts\python
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
 from app import views, models
+
