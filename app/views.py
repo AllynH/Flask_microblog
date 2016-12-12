5ec887125a74766f2014190941fdacfc1dd7f015
AllynH <AllynH@gmail.com>
2016-12-12 16:30:56 +0000

    Updating gitignore

--- app/views.py
+++ app/views.py
@@ -115,4 +115,12 @@ def edit():
 	return render_template('edit.html',
 							form=form)
 
+@app.errorhandler(404)
+def not_found_error:
+	return render_template('404.html'), 404
+	
+@app.errorhandler(500)
+def internal_error(error):
+	db.session.rollback()
+	return render_template('500.html'), 500
 
